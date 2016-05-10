068f2401025e85c5d899d7f741ba74b7ad29fb1d
Paweł Jezierski <p.jezierski999@gmail.com>
2016-05-10 21:04:09 +0200

    Initial commit

---
+++ app/src/main/java/org/example/paweljezierski/firstlauncher/AppDetail.java
@@ -0,0 +1,14 @@
+package org.example.paweljezierski.firstlauncher;
+
+/**
+ * Created by Pawel on 5/10/16.
+ */
+
+import android.graphics.drawable.Drawable;
+
+
+public class AppDetail {
+//    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
