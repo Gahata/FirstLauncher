18e166e5f2da00f6cd5b6e248f2511e7f1af95c0
Paweł <p.jezierski999@gmail.com>
2016-05-12 16:41:42 +0200

    Moving on to fragment tab view, doesnt work well currentyl

---
+++ app/src/main/java/org/example/paweljezierski/firstlauncher/AppGridFragment.java
@@ -0,0 +1,181 @@
+package org.example.paweljezierski.firstlauncher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AppGridFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AppGridFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AppGridFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AppGridFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AppGridFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AppGridFragment newInstance(String param1, String param2) {
+        AppGridFragment fragment = new AppGridFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+            loadApps();
+            loadListView();
+            addClickListener();
+        }
+    }
+
+    private PackageManager manager;
+    private List<AppDetail> apps;
+
+    private void loadApps() {
+        manager = getActivity().getPackageManager();
+        apps = new ArrayList<AppDetail>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for (ResolveInfo ri : availableActivities) {
+            AppDetail app = new AppDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+    }
+
+
+    private GridView grid;
+
+    private void loadListView() {
+        grid = (GridView) getView().findViewById(R.id.appGrid);
+
+        ArrayAdapter<AppDetail> adapter = new ArrayAdapter<AppDetail>(getActivity(),
+                R.layout.list_item, apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item, null);
+                }
+
+                ImageView appIcon = (ImageView) convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).icon);
+
+                TextView appLabel = (TextView) convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).label);
+
+//                TextView appName = (TextView)convertView.findViewById(R.id.item_app_name);
+//                appName.setText(apps.get(position).name);
+
+                return convertView;
+            }
+        };
+
+        grid.setAdapter(adapter);
+    }
+
+    private void addClickListener() {
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(pos).name.toString());
+                AppGridFragment.this.startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_app_grid, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        loadApps();
+        loadListView();
+        addClickListener();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
